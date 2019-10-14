c9476513abeed964ab0c6530fdff53276bbd49e4
Ype Kingma <ykingma@xs4all.nl>
2019-10-14 20:32:39 +0200

    WIP: record passing and failing test. Investigate...

--- phe_apl_prof.py
+++ phe_apl_prof.py
@@ -7,13 +7,20 @@ def testAdditionPerf(m, nBitSize):
     # Compute sum(1..m) via encryption and check the decryption for correctness.
     m = mpz(m)
     total = (1 + m) * m // 2 # see https://en.wikipedia.org/wiki/Triangular_number
+    print("total", total)
     pub, prv = generateKeysPaillierScheme1(nBitSize=nBitSize, useSecureRandom=False)
-    fac = pub.n // total # get many non zero bits to be encrypted
+    fac = (pub.n - 1) // total # get many non zero bits to be encrypted
+    fac = 2 ** 512 # passes, but test fails for 2 ** 1024, why?
+    print("fac", fac)
+    print("total * fac", total * fac)
     totalEnc = 1
     for i in range(1,m+1):
         enc = pub.encrypt(i * fac) # runtime is dominated by powmod call in encrypt()
         totalEnc = (totalEnc * enc) % pub.nSquared
         decTotal = prv.decrypt(totalEnc) # outside loop to test encryption only.
+        subtotal = (1 + i) * i // 2
+        assert decTotal == subtotal * fac
+    print("decTotal", decTotal)
     assert decTotal == (total * fac)
     print("testAdditionPerf passed")
 
