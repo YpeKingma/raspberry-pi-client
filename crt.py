e1751e30193f462d6c66e61a76f393a36ab54530
Ype Kingma <ykingma@xs4all.nl>
2019-10-24 18:35:20 +0200

    CRT begins to work

--- crt.py
+++ crt.py
@@ -17,34 +17,50 @@ from __future__ import print_function # for python2 development only
 Chinese remainder for constant moduli.
 """
 
+from functools import reduce
+
+def mult_inv(a, m):
+    """ Return b such that (a * b) % m == 1 """
+    # pow(a, phi(m), m) == 1, for a != 0
+    # so: b = pow(a, phi(m)-1, m)
+    # for the moment assume m is a prime with phi(m) = m - 1
+    b = pow(a, m-2, m)
+    assert (a * b) % m == 1
+    return b
+
 class CRT(object):
     def __init__(self, moduli):
         self.moduli = moduli
+        print("CRT moduli", moduli)
         self.P = reduce(lambda a, b: a*b, moduli)
+        print("CRT P", self.P)
         self.invppmds = []
         for md in self.moduli:
+            print("md", md)
             pp = self.P // md
-            inv = mul_inv(pp, md)
-            self.invppmds.append(inv * md)
+            print("pp", pp)
+            inv = mult_inv(pp, md)
+            print("inv", inv)
+            self.invppmds.append(inv * pp)
 
     def remainder(self, remainders):
         assert len(remainders) == len(self.moduli)
         s = 0
         for rem, invppmd in zip(remainders, self.invppmds):
             s += rem * invppmd
+        print("remainder sum", s)
         res = s % self.P
+        print("remainder", res)
         return res
-
 
 
 if __name__ == "__main__":
 
-    def testCRT1():
-        moduli = (2,3,7)
+    def testCRT(moduli, remainders, expected):
         crt1 = CRT(moduli)
-        remainders = (2,3,2)
         res = crt1.remainder(remainders)
-        assert res == 23
+        assert res == expected
 
-    testCRT1()
+    testCRT((3,5), (2,3), 8)
 
+    testCRT((3,5,7), (2,3,2), 23)
